db6042da8b9ed09ccd2153f5da06da65418ec331
Abao Zhang <abao@localhost.localdomain>
2017-09-03 15:14:03 +0800

    yes,在做poll2请求的时候,发现了Cookie有变化,增加了两个cookie,就是说,api变了,这真是蛋疼啊

--- mynetworkcookiejar.cpp
+++ mynetworkcookiejar.cpp
@@ -8,13 +8,22 @@
          * 因为在开发消息发送时,发生了我的小号可以发送消息,
          * 但是登录了其他号不可以发送消息,所以猜测是跟这两个数据有关
          * 猜测不能设置为不变
+         * //9.2  update
+         * pgv_pvi=729135104;
+         * pgv_si=s8344431616
          */
         QNetworkCookie  pgvCookie("pgv_info","ssid=s2345559850");
         QNetworkCookie pvid_Cookie("pgv_pvid","7745622344");
+        QNetworkCookie pgv_pvi("pgv_pvi","729135104");
+        QNetworkCookie pgv_si("pgv_si","s8344431616");
         pgvCookie.setDomain(".qq.com");
         pvid_Cookie.setDomain(".qq.com");
+        pgv_pvi.setDomain(".qq.com");
+        pgv_si.setDomain(".qq.com");
         this->insertCookie(pgvCookie);
         this->insertCookie(pvid_Cookie);
+        this->insertCookie(pgv_pvi);
+        this->insertCookie(pgv_si);
     }
 
     QList<QNetworkCookie> MyNetworkCookieJar::getCookies()
